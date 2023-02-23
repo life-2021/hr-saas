d9684285e832ca7fdafacab6a44c4b1492e926e4
life-2021 <2813901908@qq.com>
2023-02-23 13:47:06 +0800

    Changes to be committed:init 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 // ElementUI
 import ElementUI from "element-ui";
