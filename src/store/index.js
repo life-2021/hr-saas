c7f44f56db9d289b78665d4556d5af017303a573
cwk <1784539050@qq.com>
2023-03-03 18:32:58 +0800

    cwk,3.3.18.32,setting

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
-import settings from './modules/settings'
+// import settings from './modules/settings'
 import user from './modules/user'
 
 
@@ -11,7 +11,7 @@ const store = new Vuex.Store({
   // 将所有数据放在模块内部
   modules: {
     user,
-    settings
+    // settings
   },
   // 引入getters实现对子模块数据的快捷访问
   getters
---
+++ src/views/setting/assingPermission.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>分配权限功能</div>
+    <el-row slot="footer" type="flex" justify="center">
+            <el-col :span="6">
+              <el-button size="small" @click="cancelButton">取消</el-button>
+              <el-button type="primary" size="small" @click="setRolesBtn">确定</el-button>
+            </el-col>
+          </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+     name:'AssingPermission',
+     props:{
+        PermissionList:{
+            type:Array,
+            default:_=>[]
+        },
+        roleId:{
+            type:[Number,String],
+            required:true
+        }
+     },
+     data(){
+        return{}
+     },
+     created(){
+        console.log(this.roleId)
+     },
+    methods:{
+        //取消按钮
+        cancelButton(){
+            this.$emit('input',false)
+        },
+        //设置权限按钮
+        setrolesBtn(){
+            this.$emit('input',false)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.footer{
+    margin-top:30px;
+}
+</style>
