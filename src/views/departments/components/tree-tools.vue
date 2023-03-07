1b1c38edcf6f2152b4b11a8c98bc4825b18e339b
pjd <2907448043@qq.com>
2023-03-07 14:01:13 +0800

    pjd,3-7,department,permission

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px;width: 100%">
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <el-dropdown @command="operateDepts">
+            <span>
+              操作<i class="el-icon-arrow-down" />
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot" command="edit">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot" command="del">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import { delDepartments } from '../../../api/departments'
+export default {
+  props: {
+    treeNode: {
+      type: Object, 
+      required: true 
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    operateDepts(type) {
+      if (type === 'add') {
+        // 添加部门的操作
+        this.$emit('addDepts', this.treeNode)
+      } else if (type === 'edit') {
+        //  编辑部门的操作
+        this.$emit('editDepts', this.treeNode)
+      } else {
+        //  删除操作
+        this.$confirm('确定要删除该部门吗').then(() => {
+          return delDepartments(this.treeNode.id) 
+        }).then(() => {
+          this.$emit('delDepts') 
+          this.$message.success('删除部门成功')
+        })
+      }
+    }
+  }
+}
+</script>
+
